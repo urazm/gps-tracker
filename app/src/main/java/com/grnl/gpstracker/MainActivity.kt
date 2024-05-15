bade59934f70dbd2982f8a28c0c2cd187ea3ae0d
Mirlan <uraz.mir@gmail.com>
2024-05-15 20:19:58 +0300

    init

---
+++ app/src/main/java/com/grnl/gpstracker/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.grnl.gpstracker
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.grnl.gpstracker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+//    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
