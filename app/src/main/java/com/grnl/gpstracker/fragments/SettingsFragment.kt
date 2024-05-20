078a386da4c45273e0319d89116b7fd3b565278e
Mirlan <uraz.mir@gmail.com>
2024-05-20 19:27:05 +0300

    timePrev saved when change fragment

--- app/src/main/java/com/grnl/gpstracker/fragments/SettingsFragment.kt
+++ app/src/main/java/com/grnl/gpstracker/fragments/SettingsFragment.kt
@@ -9,6 +9,7 @@ import com.grnl.gpstracker.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
     private lateinit var timePreference: Preference
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.main_preference, rootKey)
         init()
@@ -16,13 +17,17 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     private fun init() {
         try {
+            Log.d("MyAppTag", "Finding preference by key: update_time_key")
             val preference = findPreference<Preference>("update_time_key")
+            val colorPreference = findPreference<Preference>("color_key")
             if (preference == null) {
                 Log.e("PREFERENCE:", "Preference not found")
             } else {
                 timePreference = preference
+                updatePreferenceTitle(timePreference)
                 val changeListener = onChangeListener()
                 timePreference.onPreferenceChangeListener = changeListener
+
             }
         } catch (e: Exception) {
             Log.e("MyAppTag", "An error occurred: ${e.message}", e)
@@ -42,4 +47,20 @@ class SettingsFragment : PreferenceFragmentCompat() {
             true
         }
     }
+
+    private fun updatePreferenceTitle(preference: Preference) {
+        val value = preference.sharedPreferences?.getString(preference.key, "")
+        if (value != null) {
+            val nameArray = resources.getStringArray(R.array.loc_time_update_names)
+            val valueArray = resources.getStringArray(R.array.loc_time_update_values)
+            val index = valueArray.indexOf(value)
+            if (index >= 0) {
+                val title = preference.title.toString().substringBefore(":")
+                preference.title = "$title: ${nameArray[index]}"
+            }
+        }
+    }
+
+
+
 }
